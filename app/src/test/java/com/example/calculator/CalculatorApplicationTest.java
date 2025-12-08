f56f7c99a8dc8fd6d113fb658cfbfd228c934e50
ccallan04 <colmcallan04@gmail.com>
2025-12-08 13:58:32 +0000

    fixing everything

---
+++ app/src/test/java/com/example/calculator/CalculatorApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CalculatorApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // If context fails to start this will fail.
+    }
+}
---
+++ app/src/test/java/com/example/calculator/IndexControllerMvcTest.java
@@ -0,0 +1,25 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IndexControllerMvcTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getRootShouldReturnOk() throws Exception {
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk());
+    }
+}
